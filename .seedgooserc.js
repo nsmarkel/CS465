9d4ff4c46ba38e8a394ae49774ab27aa7c7e8b34
nsmarkel <nsmarkel@aol.com>
2023-06-03 08:47:31 -0500

    Create new application API

--- .seedgooserc.js
+++ .seedgooserc.js
@@ -1,6 +1,6 @@
 module.exports = {
-    modelBaseDirectory: 'app_server/database/models',
+    modelBaseDirectory: 'app_api/database/models',
     models: ['*.js','!db.js'],
     data: 'data',
-    db: 'mongodb://localhost:30000/travlr'
+    db: 'mongodb://localhost:27017/travlr'
 };
--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
 const hbs = require('hbs');
+require('./app_api/database/db');
+const apiRouter = require('./app_api/routes/index');
 
 
 var app = express();
@@ -29,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+app.use('/api', apiRouter);
 
 
 // catch 404 and forward to error handler
