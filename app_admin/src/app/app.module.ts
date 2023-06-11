0b89f8ec3e7dc6aae7cf671ac1a3cc018413c87f
nsmarkel <nsmarkel@aol.com>
2023-06-10 23:05:54 -0500

    on video 3 of 6

--- app_admin/src/app/app.module.ts
+++ app_admin/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TripListingComponent } from './trip-listing/trip-listing.component';
+import { TripCardComponent } from './trip-card/trip-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TripListingComponent,
+    TripCardComponent
   ],
   imports: [
     BrowserModule
